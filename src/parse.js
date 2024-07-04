f04c0c0cafa6a3a637de57feb00906f66a36376f
Aleksey Chiglintsev <chiglincev2010@bk.ru>
2024-07-05 00:14:09 +0500

    Add mechanism for RSS parsing.

---
+++ src/parse.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+
+export default (data) => {
+  const parse = new DOMParser();
+  const doc = parse.parseFromString(data, 'application/xml');
+  if (doc.querySelector('parsererror')) {
+    throw new Error('invalidRSS');
+  }
+  const feed = {
+    title: doc.querySelector('title').textContent,
+    description: doc.querySelector('description').textContent,
+  };
+
+  const posts = Array.from(doc.querySelectorAll('item')).map((item) => {
+    const post = {
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+      id: _.uniqueId(),
+    };
+    return post;
+  });
+
+  return { feed, posts };
+};
