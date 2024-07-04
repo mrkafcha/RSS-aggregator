93138e417816f06f0d867fe4ef418d9f5ec63afd
Aleksey Chiglintsev <chiglincev2010@bk.ru>
2024-07-05 00:12:51 +0500

    Connect the i18next library

---
+++ src/locales/ru.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    invalidURL: 'Ссылка должна быть валидным URL',
+    successfully: 'RSS успешно загружен',
+    notOneOf: 'RSS уже существует',
+    invalidRSS: 'Ресурс не содержит валидный RSS',
+    networkError: 'Ошибка сети',
+    feeds: 'Фиды',
+    posts: 'Посты',
+    view: 'Просмотр',
+  },
+};
